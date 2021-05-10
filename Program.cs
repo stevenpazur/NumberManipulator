6d1093f8f9b9af37b5e355541b8f98d9772cd7f4
Steven Pazur <sjpazur@yahoo.com>
2021-05-10 13:42:22 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NumberManipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 12;
+            Console.WriteLine(myInt);
+            myInt *= myInt;
+            myInt += 14;
+            myInt -= 3;
+            Console.WriteLine(myInt);
+
+            Console.WriteLine("Please enter an integer");
+            Console.WriteLine(Method1());
+        }
+
+        public static string Method1(){
+            string input = Console.ReadLine();
+            int output;
+            if(Int32.TryParse(input, out output)){
+                output = int.Parse(input);
+            } else {
+                Console.WriteLine("Please enter a proper integer next time..");
+                return null;
+            }
+            output *= output;
+            output += 14 - 3;
+            return "The value returned is " + output;
+        }
+    }
+}
